3b95f41449a4d1ad912253044ae49507ef216e5b
Erikson Ferreira <eriksonferreira12@gmail.com>
2024-03-10 16:26:12 -0300

    fix: trying solve cors error

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ tags_metadata = [
 
 # Initialize the app
 app = FastAPI(openapi_tags=tags_metadata)
-origins = ["*"]
+origins = ["http://localhost:3000/", "200.110.205.75"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    # allow_credentials=True,
-    # allow_methods=["*"],
-    # allow_headers=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
